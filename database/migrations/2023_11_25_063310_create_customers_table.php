b848a58ade125eecff470ba1c27f32c841002367
Felipe Pessoa <felipecpessoa@outlook.com>
2023-11-25 03:44:53 -0300

    [feat] Database migrations

---
+++ database/migrations/2023_11_25_063310_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->date('birthdate');
+            $table->string('address');
+            $table->string('complement')->nullable();
+            $table->string('neighborhood');
+            $table->string('zip_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
